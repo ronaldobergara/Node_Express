77705ba55234c1fa3a169717671116eee16b3ece
ronaldobergara <bergaraheadbanger@gmail.com>
2021-02-14 22:37:51 -0300

    requisição retornando json

--- config/customExpress.js
+++ config/customExpress.js
@@ -2,9 +2,13 @@
 
 const express = require('express')
 const consign = require('consign')
+const bodyParser = require('body-parser')
 
 module.exports = () =>{
     const app = express()
+
+    app.use(bodyParser.urlencoded({extended: true}))
+    app.use(bodyParser.json())
 
     consign()
           .include('controllers')
--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,5 +1,10 @@
 // controlar as rotas do atendimento
-
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send('você esta na rota atendimento'))
+    app.get('/atendimentos', (req, res) => res.send('você esta na rota atendimento realizando um GET'))
+
+
+    app.post('/atendimentos', (req, res) => {
+        console.log(req.body)
+        res.send('Você esta na rota de atendimentos e esta realizando um post')
+    })
 }
