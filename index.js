75a373c512c11281e0055ba30166b1307b9c0a89
ronaldobergara <bergaraheadbanger@gmail.com>
2021-02-16 21:07:03 -0300

    criando conexao com BD e query

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 // responsabilidade do index.js subir servidor no ar
 const customExpress = require('./config/customExpress')
+const conexao = require('./infraestrutura/conexao')
+const tabelas = require('./infraestrutura/tabelas')
 
-const app = customExpress()
+conexao.connect(erro => {
+  if (erro){
+    console.log(erro)
+  } else{
+    console.log('conectado com sucesso!')
+    
+    tabelas.init(conexao)
+    const app = customExpress()
 
-app.listen(3000, () => console.log('servidor rodando na porta 3000'))
+    app.listen(3000, () => console.log('servidor rodando na porta 3000'))    
+  }
+})
+
